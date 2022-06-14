8c4f0c36a6e303ba2b21c673e10bf67ef216dccb
ashan0510 <asHan0510@gmail.com>
2022-06-14 17:14:37 -0400

    Code by Ashley
    CSS styling and html updates for Homepage and Donate page

--- js/index.js
+++
@@ -1,7 +0,0 @@
-$(".ToiletriesIcon").hover(function() {
-    $("#ToiletriesDefault").addClass(HiddenIcon);
-    $("#ToiletriesHover").removeClass(HiddenIcon);
-}, function() {
-    $("#ToiletriesDefault").removeClass(HiddenIcon);
-    $("#ToiletriesHover").addClass(HiddenIcon);
-});
